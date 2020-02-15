73ae4301536c0b74c807852634a70a5737baf52e
Yrix90 <Yrix90@mail.ru>
2020-02-15 23:57:13 +0700

    home

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import View
 
-def hello(request):
-    text = """<h1>Welcome</h1>"""
-    return HttpResponse(text)
+from .models import Movie
 
 
-def hello2(request, number):
-    text = """<h1> welcome2</h1>""" % number
-    
+class MoviesView(View):
+    """Список фильмов"""
+    def get(self, request):
+        movies = Movie.object.all()
+        return render(rquest, 'movies/movie_list.html', {'movies_list': movies})
